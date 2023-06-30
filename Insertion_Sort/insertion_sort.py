0afccb1df3fee2faebd3037d0350c114527e2259
Davidokih <davidyole023@gmail.com>
2023-06-30 12:28:25 +0100

    update

--- Insertion_Sort/insertion_sort.py
+++ Insertion_Sort/insertion_sort.py
@@ -1,10 +1,15 @@
 import math
 def insertion_sort(arr):
-    for i in range(len(arr)):
-        for j in range(len(arr)-i-1):
-           if(arr[j] > arr[j + 1]):
-                arr[j], arr[j + 1] = arr[j + 1], arr[j] 
+    for i in range(1,len(arr)):
+        number_to_insert = arr[i]
+        j = i - 1
+        while(j >=0 and arr[j] > number_to_insert):
+            arr[j + 1] = arr[j]
+            j = j -1
+        arr[j + 1] = number_to_insert
 
 my_list = [ 8, 20, -2, 4, -6 ]
 insertion_sort(my_list)
 print(my_list)
+
+# Big-O = O(n^2)
---
+++ Quick_Sort/quick_sort.js
@@ -0,0 +1,19 @@
+const quick_sort = (arr) => {
+    if (arr.length < 2) {
+        return arr;
+    }
+    let pivot = arr[ arr.length - 1 ];
+    let left = [];
+    let right = [];
+    for (i = 0; i < arr.length - 1; i++) {
+        if (arr[ i ] < pivot) {
+            left.push(arr[ i ]);
+        } else {
+            right.push(arr[ i ]);
+        }
+    }
+    return [ ...quick_sort(left), pivot, ...quick_sort(right) ];
+};
+
+const my_list = [ 8, 20, -2, 4, -6 ];
+console.log(quick_sort(my_list));
