753adf43f09581cb8e6f7477e1661c1d1c19f825
Davidokih <davidyole023@gmail.com>
2023-07-02 17:14:12 +0100

    update

---
+++ Marge_Sort/marge_sort.py
@@ -0,0 +1,14 @@
+import math
+
+def sort(arr):
+    for i in range(1,len(arr)):
+        number_to_insert = arr[i]
+        j = i - 1
+        while(j >= 0 and arr[j] > number_to_insert):
+            arr[j + 1] = arr[j]
+            j = j - 1
+        arr[j + 1] = number_to_insert
+            
+my_list = [ 8, 20, -2, 4, -6 ]
+sort(my_list)
+print(my_list)
