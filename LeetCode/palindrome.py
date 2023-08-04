4e6dfa86a84738bf8d7a1ccb2553609a9976c256
Davidokih <davidyole023@gmail.com>
2023-08-04 13:50:01 +0100

    update

--- LeetCode/palindrome.py
+++ LeetCode/palindrome.py
@@ -1,3 +1,6 @@
+import re
+
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
         if str(x) == str(x)[::-1]:
@@ -11,7 +14,7 @@ class Solution:
         i = m - 1
         j = n - 1
         while j >= 0:
-            print(nums1)
+            # print(nums1)
             if (i >= 0 and nums1[i] > nums2[j]):
                 nums1[constant] = nums1[i]
                 i -= 1
@@ -20,13 +23,28 @@ class Solution:
                 j -= 1
             constant -= 1
 
+    def isPalindromeString(self, s: str) -> bool:
+        punctuations = '''!()-[]{};:'`"\,<>./?@#$%^&*_~ '''
+        arr = [i for i in punctuations]
+        lowerCase = s.lower()
+        new_str = ""
+        for i in lowerCase:
+            if (i not in arr):
+                new_str += i
+
+        return new_str[::-1] == new_str
+
 
 Solution = Solution()
 
 # print(Solution.isPalindrome(121))
+print(Solution.isPalindromeString("A man, a plan, a canal: Panama"))
 nums1 = [1, 2, 3, 0, 0, 0]
 m = 3
 nums2 = [2, 5, 6]
 n = 3
 Solution.merge(nums1, m, nums2, n)
-print(nums1)
+# print(nums1)
+
+
+# print(arr)
